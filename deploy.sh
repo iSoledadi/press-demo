1ff2c156819122a6d420dbde9f3e014b4eedc1a5
ZhongJiaNing <838027059@qq.com>
2020-01-15 10:22:48 +0800

    给阿姨倒一杯卡布奇诺

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<iSoledadi>/<press-demo>.git master:gh-pages
+# git push -f git@github.com:<iSoledadi>/<press-demo>.git master:gh-pages
+git push -f https://github.com/iSoledadi/press-demo.git master:gh-pages
 
 cd -
